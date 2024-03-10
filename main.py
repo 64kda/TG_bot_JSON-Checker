e489548dfd07f729df30efdc41f568920cd4a340
Тот самый Дмитрий <151468115+64kda@users.noreply.github.com>
2024-03-10 22:21:31 +0400

    Update main.py
    1.Более удобная реализация кнопок
    2.Испсравление проблемы с зависанием
    3.Испсравления проблем со стартом.

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ import sys
 
 bot = TeleBot(token='MY_TOKEN', parse_mode='html')
 
+def generate_menu():
+    markup = types.InlineKeyboardMarkup()
+    start_button = types.InlineKeyboardButton("Начать", callback_data='start')
+    restart_button = types.InlineKeyboardButton("Перезапустить", callback_data='restart')
+    stop_button = types.InlineKeyboardButton("Остановить", callback_data='stop')
+    markup.add(start_button, restart_button, stop_button)
+
+    return markup
+
 def generate_start_message():
     markup = types.InlineKeyboardMarkup()
     restart_button = types.InlineKeyboardButton("Перезапустить", callback_data='restart')
@@ -31,54 +40,61 @@ def message_handler(message: types.Message):
         payload = json.loads(message.text)
         text = json.dumps(payload, indent=2, sort_keys=True, ensure_ascii=False)
         
-        # Добавлено сообщение об успешной обработке кода JSON
         bot.send_message(
             chat_id=message.chat.id,
-            text=f'JSON успешно проверен и отформатирован:\n<code>{text}</code>'
+            text=f'JSON успешно проверен и отформатирован:\n<code>{text}</code>',
+            reply_markup=generate_menu()
         )
     except json.JSONDecodeError:
-        # Стикер при ошибке распознавания JSON-кода
         bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAED_tdl7PHIkltFyCnwOH1jvXU7e2W-swAC9AADVp29ChFYsPXZ_VVJNAQ')
-        # Сообщение об ошибке, если JSON-код не может быть распознан
         bot.send_message(
             chat_id=message.chat.id,
-            text='Ошибка! Невозможно распознать JSON-код. Пожалуйста, убедитесь, что код введен корректно.'
+            text='Ошибка! Невозможно распознать JSON-код. Пожалуйста, убедитесь, что код введен корректно.',
+            reply_markup=generate_menu()
         )
     except Exception as e:
-        # Сообщение об ошибке в случае других ошибок
         bot.send_message(
             chat_id=message.chat.id,
-            text=f'Произошла ошибка при обработке запроса: {str(e)}'
+            text=f'Произошла ошибка при обработке запроса: {str(e)}',
+            reply_markup=generate_menu()
         )
 
 def handle_file(message: types.Message):
-    # Сообщение об ошибке при отправке файла
     bot.send_message(
         chat_id=message.chat.id,
-        text='Извините, в данном боте не предусмотрена обработка файлов. Пожалуйста, отправьте JSON в виде строки текста.'
+        text='Извините, в данном боте не предусмотрена обработка файлов. Пожалуйста, отправьте JSON в виде строки текста.',
+        reply_markup=generate_menu()
     )
 
 @bot.callback_query_handler(func=lambda call: call.data == 'restart')
 def handle_restart(call: types.CallbackQuery):
-    # ваш код обработки команды restart
-    bot.send_sticker(call.message.chat.id, 'CAACAgIAAxkBAAED_qhl7OhYcbq65VLyLtFCzTwHEp5HtAACAgEAAladvQpO4myBy0Dk_zQE')  # отправка нового стикера
+    bot.send_sticker(call.message.chat.id, 'CAACAgIAAxkBAAED_qhl7OhYcbq65VLyLtFCzTwHEp5HtAACAgEAAladvQpO4myBy0Dk_zQE')
     bot.send_message(
         chat_id=call.message.chat.id,
-        text='Бот был перезапущен!'
+        text='Бот был перезапущен!',
+        reply_markup=generate_menu()
     )
-    pass  # этот pass всегда можно заменить реальным кодом
 
 @bot.callback_query_handler(func=lambda call: call.data == 'stop')
 def handle_stop(call: types.CallbackQuery):
-    bot.send_sticker(call.message.chat.id, 'CAACAgIAAxkBAAED_pxl7OQjfvnoDPpYaYzmF8l_46wgWwACDgEAAladvQoRqS1ownHgaDQE')  # отправка стикера
+    bot.send_sticker(call.message.chat.id, 'CAACAgIAAxkBAAED_pxl7OQjfvnoDPpYaYzmF8l_46wgWwACDgEAAladvQoRqS1ownHgaDQE')
     bot.send_message(
         chat_id=call.message.chat.id,
-        text='Бот был остановлен.'
+        text='Бот был остановлен.',
+        reply_markup=generate_menu()
     )
-    # Завершаем выполнение программы
     sys.exit()
 
+@bot.callback_query_handler(func=lambda call: call.data == 'start')
+def handle_start(call: types.CallbackQuery):
+    bot.send_message(
+        chat_id=call.message.chat.id,
+        text='Добро пожаловать! Введите JSON для проверки.',
+        reply_markup=None
+    )
+
 def main():
+    bot.send_message(chat_id='319922443', text='Выберите действие:', reply_markup=generate_menu())
     bot.infinity_polling()
 
 if __name__ == '__main__':
