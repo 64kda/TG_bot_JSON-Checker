2ab115c881c2e7d35ccf2c1ddff13b3923d357c5
Тот самый Дмитрий <151468115+64kda@users.noreply.github.com>
2024-03-11 21:15:44 +0400

    Update main.py
    Добавлен стикер при старте бота

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
 from telebot import TeleBot, types
 import json
-import sys
 from time import sleep
+from html.parser import HTMLParser
 
 # Замените 'YOUR_TOKEN' на ваш реальный токен
 bot = TeleBot(token='YOUR_TOKEN', parse_mode='html')
 
-# Функция для генерации основного меню с кнопками "Начать" и "Перезапустить"
+# Класс-парсер для определения, является ли текст HTML
+class HTMLTextParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.is_html = False
+
+    def handle_starttag(self, tag, attrs):
+        self.is_html = True
+
+    def handle_data(self, data):
+        pass
+
+    def handle_endtag(self, tag):
+        pass
+
+# Функция для генерации основного меню с кнопкой "Начать"
 def generate_menu():
     markup = types.InlineKeyboardMarkup()
     start_button = types.InlineKeyboardButton("Начать", callback_data='start')
-    restart_button = types.InlineKeyboardButton("Перезапустить", callback_data='restart')
-    markup.add(start_button, restart_button)
+    markup.add(start_button)
 
     return markup
 
-# Функция для генерации стартового сообщения с кнопкой "Перезапустить"
+# Функция для генерации стартового сообщения с кнопкой "Начать"
 def generate_start_message():
     markup = types.InlineKeyboardMarkup()
-    restart_button = types.InlineKeyboardButton("Перезапустить", callback_data='restart')
-    markup.add(restart_button)
+    start_button = types.InlineKeyboardButton("Начать", callback_data='start')
+    markup.add(start_button)
 
     return 'Привет! Я умею проверять JSON и форматировать его в красивый текст. Введи JSON в виде строки или отправь файл с JSON:', markup
 
@@ -41,7 +55,7 @@ def message_handler(message: types.Message):
     try:
         # Проверяем, является ли сообщение файлом, и обрабатываем его, если да
         if message.document or message.photo or message.video or message.voice or message.audio or message.sticker or message.animation or message.video_note or message.contact or message.location or message.venue or message.dice:
-            handle_file(message)
+            handle_media(message)
             return
 
         # Пытаемся обработать текстовое сообщение как JSON
@@ -57,11 +71,22 @@ def message_handler(message: types.Message):
     except json.JSONDecodeError:
         # В случае ошибки при распознавании JSON отправляем стикер и сообщение с ошибкой
         bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAED_tdl7PHIkltFyCnwOH1jvXU7e2W-swAC9AADVp29ChFYsPXZ_VVJNAQ')
-        bot.send_message(
-            chat_id=message.chat.id,
-            text='Ошибка! Невозможно распознать JSON-код. Пожалуйста, убедитесь, что код введен корректно.',
-            reply_markup=generate_menu()
-        )
+        
+        # Добавляем обработку HTML кода
+        html_parser = HTMLTextParser()
+        html_parser.feed(message.text)
+        if html_parser.is_html:
+            bot.send_message(
+                chat_id=message.chat.id,
+                text='Ошибка! Введенный текст похож на HTML, пожалуйста, введите JSON.',
+                reply_markup=generate_menu()
+            )
+        else:
+            bot.send_message(
+                chat_id=message.chat.id,
+                text='Ошибка! Невозможно распознать JSON-код. Пожалуйста, убедитесь, что код введен корректно.',
+                reply_markup=generate_menu()
+            )
     except Exception as e:
         # В случае других ошибок отправляем сообщение с ошибкой и основное меню с кнопками
         bot.send_message(
@@ -71,13 +96,25 @@ def message_handler(message: types.Message):
         )
 
 # Обработчик для файлов, отправленных пользователем
-def handle_file(message: types.Message):
-    # Отправляем сообщение об ошибке при отправке файла и основное меню с кнопками
-    bot.send_message(
-        chat_id=message.chat.id,
-        text='Извините, в данном боте не предусмотрена обработка файлов. Пожалуйста, отправьте JSON в виде строки текста.',
-        reply_markup=generate_menu()
-    )
+def handle_media(message: types.Message):
+    # Обработка изображений
+    if message.photo:
+        bot.send_message(
+            chat_id=message.chat.id,
+            text='Это изображение! Спасибо за отправку.'
+        )
+    # Обработка стикеров
+    elif message.sticker:
+        bot.send_message(
+            chat_id=message.chat.id,
+            text='Это стикер! Спасибо за отправку.'
+        )
+    # Обработка остальных медиафайлов
+    else:
+        bot.send_message(
+            chat_id=message.chat.id,
+            text='Спасибо за отправку файла!'
+        )
 
 # Обработчик нажатия на кнопку "Перезапустить"
 @bot.callback_query_handler(func=lambda call: call.data == 'restart')
@@ -93,6 +130,8 @@ def handle_restart(call: types.CallbackQuery):
 # Обработчик нажатия на кнопку "Начать"
 @bot.callback_query_handler(func=lambda call: call.data == 'start')
 def handle_start(call: types.CallbackQuery):
+    # Отправляем стикер
+    bot.send_sticker(call.message.chat.id, 'CAACAgIAAxkBAAEEB65l7zcXYTjLaiAkDTsKx66XqhljIQACTgIAAladvQow_mttgTIDbzQE')
     # Отправляем приветственное сообщение без клавиатуры
     bot.send_message(
         chat_id=call.message.chat.id,
@@ -106,7 +145,9 @@ def main():
         try:
             bot.polling(none_stop=True)
         except Exception as e:
+            # В случае ошибки при работе бота выводим сообщение об ошибке в консоль
             print(f'Ошибка при работе бота: {e}')
+            # Ждем некоторое время перед повторной попыткой
             sleep(0.3)
 
 # Запуск бота
